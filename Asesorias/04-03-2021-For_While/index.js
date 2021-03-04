dd86a39cdcdd5681723155f70300874831144578
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-04 13:52:07 -0600

    Ciclos

---
+++ Asesorias/04-03-2021-For_While/index.js
@@ -0,0 +1,28 @@
+console.log("Hola mundo")
+
+//FOR
+
+// for(var i = 1 ; i <= 100 ; i++ ){
+
+//     if(i % 2 == 0 ){
+//         console.log("SOY PAR",i)
+//     }
+
+// }
+
+
+// 20 / 2 = 10  Residuo = 0
+// 21 / 2 = 10  Residuo = 1
+
+
+// 1+2+3+4+5+6+......99+100
+var suma = 0
+for(var contador = 1 ; contador <= 100 ; contador++ ){
+            //0       //1
+    suma  = suma  + contador;
+
+    console.log("Suma hasta el momento",suma,"#",contador)
+    // 1 = 1 + 2
+    // 3 = 3 + 3
+}
+console.log(suma)
