d691f76a8c37a0db3bc41d6a42615b9e934369ef
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-08 14:19:12 -0600

    Ciclos

---
+++ Asesorias/03-08-2021-Ciclos/index.js
@@ -0,0 +1,50 @@
+//Ciclos
+
+
+// contador, condicion, incremento
+
+// for(var contador = 0 ; contador < 100 ; contador++){
+//     if(caja <= "30x30"){
+//         console.log("ESTA CAJA SI ME SIRVE")
+//     }
+// }
+
+
+
+// if(caja1 <= "30x30"){
+//     console.log("ESTA CAJA SI ME SIRVE")
+// }
+// if(caja2 <= "30x30"){
+//     console.log("ESTA CAJA SI ME SIRVE")
+// }
+
+// if(caja3 <= "30x30"){
+//     console.log("ESTA CAJA SI ME SIRVE")
+// }
+
+
+for(var contador = 0 ; contador < 100 ; contador++){
+    if(contador % 2 === 1){
+        console.log(contador,"PRIMO")
+    }
+    if( contador % 3 === 0){
+        console.log(contador,"ES DIVISIBLE ENTRE 3")
+    }
+}
+
+
+
+// 60/3 = 20  res = 0
+
+// 3/3 = 1  residuo = 0
+// 3/1 = 3  residuo = 0
+// 3/2 = 1  residuo = 1
+
+// 4/4 = 1  residuo = 0
+// 4/1 = 4  residuo = 0
+// 4/2 = 2  residuo = 0
+
+// 5/5 = 1  residuo = 0
+// 5/1 = 5  residuo = 0
+// 5/2 = 2  residuo = 1
+
