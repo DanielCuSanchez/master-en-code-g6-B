f96a864fadc14f774d34eb81b331455224d45db3
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-09 17:20:56 -0600

    Sumas

---
+++ Asesorias/09-03-2021-Funciones/index.js
@@ -0,0 +1,15 @@
+
+//Funcion -> bloque de codigo que resuelve un problema
+
+
+
+function nombreDeLaFuncion(parametro1, parametro2){
+    console.log(parametro1, parametro2);
+}
+function nombreDeLaFuncion(){
+    console.log("Hola");
+}
+
+
+
+nombreDeLaFuncion()
