21015dd865856dba07d823986daff5c33c55e375
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-12 19:35:26 -0600

    Condicionales

---
+++ JavaScript_desde_0/1-Condicionales/index.js
@@ -0,0 +1,15 @@
+//Condicinales
+let string = prompt("Ingresa un numero ")
+let numero = parseInt(string)
+
+
+if( numero == 0 ){
+    console.log("YEI, SOY EL 0");
+}else if(numero == 1){
+    console.log("YEI, SOY EL 1");
+}else if(numero == 2){
+    console.log("YEI, SOY EL 2");
+}
+
+alert( parseInt(string) )
+
