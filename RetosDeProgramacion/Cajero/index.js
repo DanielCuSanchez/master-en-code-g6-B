e1506a30e752451833bd15eb66af7aab8165a700
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-17 00:17:50 -0600

    Login y estilo reto cajero

--- RetosDeProgramacion/Cajero/index.js
+++ RetosDeProgramacion/Cajero/index.js
@@ -6,27 +6,55 @@ const cuentasUsuario = [
 ]
 
 const contenedorInfoUsuario = document.getElementById("contenedorInfoUsuario")
-const inputUsuario = document.getElementById("inputUsuario").value
-const inputPassword = document.getElementById("inputPassword").value
 const botonValidarLogin = document.getElementById("botonValidarLogin")
+const inputUsuario = document.getElementById("inputUsuario")
+const inputPassword = document.getElementById("inputPassword")
+
+//Eventos
+botonValidarLogin.addEventListener("click",inicioDePrograma)
+
+//Loop de programa
+
+function inicioDePrograma(){
+  if(validarLogin()){
+    ocultarElementos([inputUsuario, inputPassword, botonValidarLogin])
+  }else{
+    contenedorInfoUsuario.style.display = "block"
+    contenedorInfoUsuario.innerHTML = "Credenciales incorrectas"
+    setTimeout(function(){
+      contenedorInfoUsuario.style.display = "none"
+      contenedorInfoUsuario.innerHTML = ""
+      
+    },1000)
+  }
+}
+
+//Funciones
+
+function ocultarElementos(elementos){
+  for (let contador = 0; contador < elementos.length; contador++) {
+    elementos[contador].style.display="none"
+  }
+}
 
 
-botonValidarLogin.addEventListener("click",validarLogin)
-
-console.log(botonValidarLogin.addEventListener("click",validarLogin))
+function mostrarElementos(elementos){
+  for (let contador = 0; contador < elementos.length; contador++) {
+    elementos[contador].style.display="block"
+  }
+}
 
 
 function validarLogin(){
-  console.log("Entre");
-  console.log(inputUsuario)
-  console.log(inputPassword)
   for (let contador = 0; contador < cuentasUsuario.length; contador++) {
-    if(inputUsuario === cuentasUsuario[contador].nombre &&  inputPassword === cuentasUsuario[contador].password ){
+    if(inputUsuario.value === cuentasUsuario[contador].nombre &&  inputPassword.value === cuentasUsuario[contador].password ){
       alert("Logueado")
       return true
     }
   }
+  contenedorInfoUsuario.innerHTML = "Credenciales incorrectas"
   return false
 }
 
 //console.log(validarLogin())
+
