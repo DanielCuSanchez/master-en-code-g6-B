b65d3a0f291f5d3454f397793707402bab7ecf89
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-03-18 20:24:17 -0600

    Cajero

--- RetosDeProgramacion/Cajero/index.js
+++ RetosDeProgramacion/Cajero/index.js
@@ -43,22 +43,29 @@ const botonConsultarSaldo = document.getElementById("botonConsultarSaldo")
 const botonDepositar = document.getElementById("botonDepositar")
 const botonRetirar = document.getElementById("botonRetirar")
 const botonCerrarSesion = document.getElementById("botonCerrarSesion")
+const nombreCliente = document.getElementById("nombreCliente")
 
 function inicioDeSesion(){
+
+  const posicionExtraida = extraerPosicion()
+
+  nombreCliente.innerHTML = "Bienvenid@ " + cuentasUsuario[posicionExtraida].nombre
+  contenedorInfoUsuario.innerHTML = "Selecciona una opción"
+
   ocultarElementos([inputUsuario, inputPassword, botonValidarLogin])
-  mostrarElementos([menuCajero,botonCerrarSesion, contenedorInfoUsuario])
+  mostrarElementos([menuCajero,botonCerrarSesion, contenedorInfoUsuario, nombreCliente])
 
   botonConsultarSaldo.addEventListener("click",consultarSaldo)
   botonDepositar.addEventListener("click", depositarAlaCuenta)
   botonRetirar.addEventListener("click",retirarDeLaCuenta)
-
+  botonCerrarSesion.addEventListener("click",cerrarSesion)
 }
 
 //Opciones de menu
 
 function consultarSaldo(){
   const posicionExtraida = extraerPosicion()
-  contenedorInfoUsuario.innerHTML = "Saldo: " + cuentasUsuario[posicionExtraida].saldo
+  contenedorInfoUsuario.innerHTML = "Saldo: $" + cuentasUsuario[posicionExtraida].saldo
 }
 
 function depositarAlaCuenta(){
@@ -70,21 +77,53 @@ function depositarAlaCuenta(){
     //Añaden dinero al usuario
     cuentasUsuario[posicionExtraida].saldo = cuentasUsuario[posicionExtraida].saldo + inputDinero
     //Mostramos el nuevo saldo en la caja de texto
-    contenedorInfoUsuario.innerHTML = "Nuevo Saldo: " + cuentasUsuario[posicionExtraida].saldo
+    contenedorInfoUsuario.innerHTML = "Nuevo Saldo: $" + cuentasUsuario[posicionExtraida].saldo
   }else{
     contenedorInfoUsuario.innerHTML = "Ingresa saldo positivo"
+    setTimeout(()=>{
+      contenedorInfoUsuario.innerHTML = "Selecciona una opción"
+    },3000)
   }
 }
 
 function retirarDeLaCuenta(){
   const posicionExtraida = extraerPosicion()
   const inputDinero = parseInt(prompt("Cantidad a retirar"))
+  if( inputDinero < 0){
+    contenedorInfoUsuario.innerHTML = "Ingresa mayor a 0"
+    setTimeout(()=>{
+      contenedorInfoUsuario.innerHTML = "Selecciona una opción"
+    },3000)
+
+    return
+  }
+
+  if( inputDinero > cuentasUsuario[posicionExtraida].saldo){
+    contenedorInfoUsuario.innerHTML = "No tienes suficiente saldo"
+    setTimeout(()=>{
+      contenedorInfoUsuario.innerHTML = "Selecciona una opción"
+    },3000)
+
+    return
+  }
+
+
   if(inputDinero <= cuentasUsuario[posicionExtraida].saldo){
     //Quitar dinero al usuario
     cuentasUsuario[posicionExtraida].saldo = cuentasUsuario[posicionExtraida].saldo - inputDinero
     //Mostramos el nuevo saldo en la caja de texto
-    contenedorInfoUsuario.innerHTML = "Nuevo Saldo: " + cuentasUsuario[posicionExtraida].saldo
+    contenedorInfoUsuario.innerHTML = "Nuevo Saldo: $" + cuentasUsuario[posicionExtraida].saldo
+    return
   }
+}
+
+//Cerrar Sesion
+
+function cerrarSesion(){
+  ocultarElementos([menuCajero,botonCerrarSesion, contenedorInfoUsuario, nombreCliente])
+  mostrarElementos([inputUsuario, inputPassword, botonValidarLogin])
+  inputUsuario.value = ""
+  inputPassword.value = ""
 }
 
 //Funciones auxiliares
