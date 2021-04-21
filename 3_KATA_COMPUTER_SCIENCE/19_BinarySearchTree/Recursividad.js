a2a29e44d790d57a45d8bafaf696b4ae1b724332
danielcu <danielcu.sanchez@gmail.com>
2021-04-20 19:47:27 -0500

    Explicacion recursion

--- 3_KATA_COMPUTER_SCIENCE/19_BinarySearchTree/Recursividad.js
+++ 3_KATA_COMPUTER_SCIENCE/19_BinarySearchTree/Recursividad.js
@@ -19,18 +19,40 @@
 // }
 
 // Ejemplo de función de Fibonacci RECURSIVA
+
+/**
+ *
+ *
+ * La sucesión comienza con los números 0 y 1;
+ * 2​ a partir de estos
+ * «cada término es la suma de los dos anteriores»,
+ */
 function fibo(num) {
+  debugger;
+  //1
   if (num < 2) {
+    //1<2
     return num;
   } else {
-    return fibo(num - 1) + fibo(num - 2);
+    return fibo(num - 1) + fibo(num - 2); //return 2
+    //3-1=2 regreso=1   3-2=1 regreso 1
+    //2-1=1 regreso=1
+    //2-2=0 regreso=0
   }
 }
 
-// Creando arreglo con serie de Fibonacci
+//Creando arreglo con serie de Fibonacci
 numeros = [];
 let nTerms = 13;
 for (let i = 0; i <= nTerms; i++) {
   numeros.push(fibo(i));
 }
 console.log(numeros);
+
+console.log(fibo(0));
+console.log(fibo(1));
+console.log(fibo(2));
+console.log(fibo(3));
+console.log(fibo(4));
+console.log(fibo(5));
+console.log(fibo(6));
