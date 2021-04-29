bf1264f4009c0a7545e9045e1e13ef225830053e
danielcu <danielcu.sanchez@gmail.com>
2021-04-28 19:36:11 -0500

    funcion factorial recursiva

---
+++ 3_KATA_COMPUTER_SCIENCE/19_Recursion/factorial.js
@@ -0,0 +1,12 @@
+const factorial = (numero) => {
+  //5
+  //4  //3 //2 //1
+  if (numero === 0) {
+    return 1; //0
+  }
+  return numero * factorial(numero - 1); //5 * //4 (24)  //3 (6) //2 (2) //1 (1) //0 (1)
+};
+
+const resultado = factorial(5);
+
+console.log(resultado);
