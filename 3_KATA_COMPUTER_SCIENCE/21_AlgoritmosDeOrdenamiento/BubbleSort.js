ff0bdb932d45668efcb6cedfb5ccc7dd80f720b8
danyparc <danyparc@gmail.com>
2021-04-29 19:43:12 -0500

    update BubbleSort

--- 3_KATA_COMPUTER_SCIENCE/21_AlgoritmosDeOrdenamiento/BubbleSort.js
+++ 3_KATA_COMPUTER_SCIENCE/21_AlgoritmosDeOrdenamiento/BubbleSort.js
@@ -1,20 +1,27 @@
 let numeros = [
-  2, 1, 3, 22, 7, 5, 0, 9, 4, 25
+    2, 1, 3, 22, 7, 5, 0, 9, 4, 25
 ]
+
+// Función que recibe dos indices e intercambia valores en un arreglo
+function swap(indexA, indexB, arreglo){
+  const temp = arreglo[indexA]
+  arreglo[indexA] = arreglo[indexB]
+  arreglo[indexB] = temp
+  return arreglo
+}
+
 
 function BubbleSort (arreglo) {
   let operaciones = 0
   let long = arreglo.length // cantidad de numeros a considerar para comparación
   for (let i = 0; i < arreglo.length; i++) {
-    long--
     for (let j = 0; j < long; j++) {
       if(arreglo[j] > arreglo[j+1]){
-        const temp = arreglo[j]
-        arreglo[j] = arreglo[j+1]
-        arreglo[j+1] = temp
+        swap(j, j+1, arreglo)
       }
       operaciones++
     }
+    long-- // Nos ayuda a ya no considerar el numero que fue ordenado en el for interno 
   }
   console.log('Se hicieron:', operaciones, 'operaciones');
   return arreglo
