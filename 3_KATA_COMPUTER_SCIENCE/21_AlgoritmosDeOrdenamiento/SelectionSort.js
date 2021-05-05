63d1064d4f745c9e0bc4b7271de68e65e25ceeb7
danielcu <danielcu.sanchez@gmail.com>
2021-05-04 22:11:11 -0500

    merge

--- 3_KATA_COMPUTER_SCIENCE/21_AlgoritmosDeOrdenamiento/SelectionSort.js
+++ 3_KATA_COMPUTER_SCIENCE/21_AlgoritmosDeOrdenamiento/SelectionSort.js
@@ -1,35 +1,35 @@
-let numeros = [
-    2, 1, 3, 22, 7, 5, 0, 9, 4, 25
-]// 0, 1, 2, 3,  4, 5, 6, 7, 8, 9
-
-// numeros[indexMin] = numeros[0] = 2
-// numeros[indexMin] = numeros[1] = 1
-// numeros[indexMin] = numeros[6] = 0
-
-function SelectionSort(arreglo){
-  let operaciones = 0
-
-  for(i=0; i < arreglo.length; i++){
-    let indexMin = i // posición del número menor (asumimos que es el primer número)
-    for(j=i; j < arreglo.length; j++){
-      if(arreglo[i] > arreglo[j]){
-        indexMin = j
-      }
-      operaciones++
-    }
-
-    if(indexMin !== i){ // Hacemos intercambio de valores (swap)
-      let temp = arreglo[i]
-      arreglo[i] = arreglo[indexMin]
-      arreglo[indexMin] = temp
-    }
-
-  }
-  console.log('Se hicieron', operaciones, 'operaciones');
-  return arreglo
+// Función que recibe dos indices e intercambia valores en un arreglo
+function swap(indexA, indexB, arreglo) {
+  const temp = arreglo[indexA];
+  arreglo[indexA] = arreglo[indexB];
+  arreglo[indexB] = temp;
+  return arreglo;
 }
 
-console.log('Arreglo antes:', numeros)
-numeros = SelectionSort(numeros)
-console.log('Arreglo después:', numeros)
+function SelectionSort(arreglo) {
+  for (indexBarrita = 0; indexBarrita < arreglo.length; indexBarrita++) {
+    let indexMin = indexBarrita; // posición del número menor (asumimos que es el primer número)
 
+    for (
+      indexCirculito = indexBarrita;
+      indexCirculito < arreglo.length;
+      indexCirculito++
+    ) {
+      if (arreglo[indexBarrita] > arreglo[indexCirculito]) {
+        indexMin = indexCirculito;
+      }
+    }
+
+    if (indexMin !== indexBarrita) {
+      // Hacemos intercambio de valores (swap)
+      swap(indexBarrita, indexMin, arreglo);
+    }
+  }
+  return arreglo;
+}
+
+let numeros = [0, 2, 3, 5, 7, 22]; // 0, 1, 2, 3, 4, 5, <- indices
+
+console.log("Arreglo antes:", numeros);
+numeros = SelectionSort(numeros);
+console.log("Arreglo después:", numeros);
