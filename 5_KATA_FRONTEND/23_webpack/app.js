d3d43eaf4a25e4ea3c12055b44df9cf8ef251197
danielcu <danielcu.sanchez@gmail.com>
2021-05-21 20:59:04 -0500

    webpack

--- 5_KATA_FRONTEND/23_webpack/app.js
+++ 5_KATA_FRONTEND/23_webpack/app.js
@@ -3,5 +3,6 @@ const h1 = document.getElementById("name");
 
 alert("hola");
 h1.innerHTML = "Hola mundo desde JS";
-
 saludar("mundo");
+
+console.log(":)");
---
+++ 5_KATA_FRONTEND/23_webpack/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+// ./configs
+
+//path.join(__dirname, "")
+
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "/dist"),
+  },
+  mode: "development",
+};
