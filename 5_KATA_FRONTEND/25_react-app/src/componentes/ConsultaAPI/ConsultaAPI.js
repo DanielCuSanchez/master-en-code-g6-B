6a6ba86afffef21320320d1ec0ae04709eab53ef
daniel-gm <daniel.gm@globant.com>
2021-06-02 20:49:58 -0500

    improve ConsultaAPI

--- 5_KATA_FRONTEND/25_react-app/src/componentes/ConsultaAPI/ConsultaAPI.js
+++ 5_KATA_FRONTEND/25_react-app/src/componentes/ConsultaAPI/ConsultaAPI.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment , useEffect, useState } from "react";
 import { Card } from "./../Card/Card";
 
 export const ConsultaAPI = () => {
   const URL = "https://rickandmortyapi.com/api/character";
   const [personajes, setPersonajes] = useState([]);
+  const [personajesFiltrados, setPersonajesFiltrados] = useState([]);
 
   useEffect(() => {
     fetch(URL)
@@ -13,6 +14,7 @@ export const ConsultaAPI = () => {
       })
       .then((data) => {
         setPersonajes(data.results);
+        setPersonajesFiltrados(data.results);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -24,18 +26,62 @@ export const ConsultaAPI = () => {
     setPersonajes(nuevosPersonajes);
   };
 
+  const filterRick = () => {
+    setPersonajesFiltrados(
+      personajes.filter((personaje) => personaje.name.includes("Rick"))
+    );
+  };
+
+  const filterMortys = () => {
+    setPersonajesFiltrados(
+      personajes.filter((personaje) => personaje.name.includes("Morty"))
+    );
+  };
+
+  const handlerTodos = () => {
+    setPersonajesFiltrados(personajes);
+  };
+
+  const titleStyles = personajes.length > 0  ? {marginTop: '32px', color: 'red'} : {}
+
   return (
     <div>
       {console.log(personajes)}
-      <h2>Consulta a la API de rick and morty </h2>
+      <h2 style={titleStyles} >Consulta a la API de rick and morty </h2>
       <div className="container">
-        <div className="row">
-          {personajes.map((personaje) => (
-            <div key={personaje.id} className="col-md-3 my-2">
-              <Card {...personaje} handlerRemove={handlerRemove} />
+        {personajes.length === 0 ? (
+          <div className="row justify-content-center">
+            <div className="col">
+              <div class="d-flex align-items-center">
+                <strong>Loading...</strong>
+                <div
+                  class="spinner-border ms-auto"
+                  role="status"
+                  aria-hidden="true"
+                ></div>
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ) : (
+          <Fragment>
+            <button className="btn btn-primary" onClick={handlerTodos}>
+              Todos
+            </button>
+            <button className="btn btn-primary" onClick={filterRick}>
+              Solo Ricks
+            </button>
+            <button className="btn btn-primary" onClick={filterMortys}>
+              Solo Mortys
+            </button>
+            <div className="row">
+              {personajesFiltrados.map((personaje) => (
+                <div key={personaje.id} className="col-md-3 my-2">
+                  <Card {...personaje} handlerRemove={handlerRemove} />
+                </div>
+              ))}
+            </div>
+          </Fragment>
+        )}
       </div>
     </div>
   );
