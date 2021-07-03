622ceecf6260707da7591e3a2f804ad086efb404
danielcu <danielcu.sanchez@gmail.com>
2021-07-02 22:27:24 -0500

    funcion crea estado inicial formulario

--- 5_KATA_FRONTEND/27_forms-react/src/Componentes/FormikForm.js
+++ 5_KATA_FRONTEND/27_forms-react/src/Componentes/FormikForm.js
@@ -3,18 +3,27 @@ import { Formik, Form, Field } from "formik";
 import { preguntas } from "./preguntas.json";
 
 export const FormikForm = () => {
-  console.log(preguntas);
-  const estadoInicial = { pregunta1: "", pregunta2: "" };
+  const creaEstadoIncial = () => {
+    let estadoInicial = {};
+    Object.keys(preguntas).forEach((key) => {
+      estadoInicial = {
+        ...estadoInicial,
+        [preguntas[key].name]: "",
+      };
+    });
+    return estadoInicial;
+  };
 
   const funcionQueValida = (values) => {
     console.log(values);
   };
 
   return (
-    <div className="row justify-content-center">
-      <div className="col-md-6">
+    <div className="row justify-content-center text-center">
+      <div className="col-md-8">
         <h1>Sign Up</h1>
-        <Formik initialValues={estadoInicial} onSubmit={funcionQueValida}>
+
+        <Formik initialValues={creaEstadoIncial()} onSubmit={funcionQueValida}>
           <Form>
             {preguntas.map((pregunta) => {
               return (
