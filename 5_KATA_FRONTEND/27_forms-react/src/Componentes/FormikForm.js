d3081ddd1f35c1616d31ba044a3af6d50a4f8c3b
danielcu <danielcu.sanchez@gmail.com>
2021-07-02 22:09:01 -0500

    formValues

--- 5_KATA_FRONTEND/27_forms-react/src/Componentes/FormikForm.js
+++ 5_KATA_FRONTEND/27_forms-react/src/Componentes/FormikForm.js
@@ -5,17 +5,16 @@ import { preguntas } from "./preguntas.json";
 export const FormikForm = () => {
   console.log(preguntas);
   const estadoInicial = { pregunta1: "", pregunta2: "" };
+
+  const funcionQueValida = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-6">
         <h1>Sign Up</h1>
-        <Formik
-          initialValues={estadoInicial}
-          onSubmit={async (values) => {
-            await new Promise((r) => setTimeout(r, 500));
-            alert(JSON.stringify(values, null, 2));
-          }}
-        >
+        <Formik initialValues={estadoInicial} onSubmit={funcionQueValida}>
           <Form>
             {preguntas.map((pregunta) => {
               return (
