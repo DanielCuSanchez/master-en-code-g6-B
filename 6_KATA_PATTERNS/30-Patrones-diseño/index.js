ccc41953b35cf32af85ea9fb39dffdfcb8033ccd
Daniel Cu Sanchez <danielcu@alternet.com.mx>
2021-08-12 21:26:41 -0500

    Patrones de diseño

---
+++ 6_KATA_PATTERNS/30-Patrones-diseño/index.js
@@ -0,0 +1,88 @@
+//Ejemplo de object prototype
+
+Object.prototype.logMio = () => {
+  console.log(this);
+};
+
+const objeto = {
+  nombre: "Sergio",
+  edad: 25,
+};
+
+const objeto2 = {
+  nombre: "Carlos",
+  edad: 26,
+};
+
+console.log(objeto);
+console.log(objeto2);
+
+//Prototypes
+
+let creadoraDeNuevaVariable = function () {};
+
+creadoraDeNuevaVariable.prototype.saludo = "Hola";
+
+//console.log(creadoraDeNuevaVariable.prototype);
+
+const nuevaVariable = new creadoraDeNuevaVariable();
+
+//console.log(nuevaVariable.saludo);
+
+let Cliente = function () {};
+
+Cliente.prototype.name = "Default name";
+
+Cliente.prototype.setName = function (name) {
+  this.name = name;
+};
+
+Cliente.prototype.getName = function () {
+  return this.name;
+};
+
+let clienteJuan = new Cliente();
+
+//console.log(Cliente.prototype);
+
+//clienteJuan._prototype_.name = "Juan";
+
+//console.log(Cliente.prototype);
+
+//Clases
+
+class ClienteClase {
+  constructor(name, edad) {
+    this.name = name;
+    this.edad = edad;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+const nuevoCliente = new ClienteClase("David", 22);
+
+console.log(nuevoCliente);
+
+class creaNuevoClienteEcommerce {
+  constructor(nombre, edad) {
+    this.cliente = new ClienteClase(nombre, edad);
+  }
+}
+class creaNuevoClienteDb {
+  constructor(nombre, edad) {
+    this.cliente = new ClienteClase(nombre, edad);
+  }
+}
+class creaNuevoClienteJs {
+  constructor(nombre, edad) {
+    this.cliente = new ClienteClase(nombre, edad);
+  }
+}
+
+//Funcional
+function crearCliente(nombre, edad) {
+  return new ClienteClase(nombre, edad);
+}
